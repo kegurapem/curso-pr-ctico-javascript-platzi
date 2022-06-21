fa2a76b52d83321ab138d7ff8cc1dd505ecda059
Kevin Harold Gutierrez Ramirez <harold.dev1993@gmail.com>
2022-06-21 16:41:45 -0500

    agregando arrays inteligentes al proyecto de descuento

--- descuento.js
+++ descuento.js
@@ -1,14 +1,48 @@
-// const precioOriginal = 100;
-// const descuento = 15;
-
 function calcularPrecioConDescuento(precio, descuento){
     const porcentajePrecioConDescuento = 100 - descuento;
     const precioConDescuento = (precio * porcentajePrecioConDescuento) / 100;
 
     return precioConDescuento;
 }
-aa
 
 
-// console.log("El precio original es: " + precioOriginal)
-// git p[]
+
+
+function onClickButtonPriceDiscount() {
+    const coupons = [
+        {
+            name: "JuanDC_es_Batman",
+            discount: 15,
+        },
+        {
+            name: "pero_no_le_digas_a_nadie",
+            discount: 30,
+        },
+        {
+            name: "es_un_secreto",
+            discount: 25,
+        },
+    ];
+
+    const inputPrice = document.getElementById("InputPrice");
+    const priceValue = inputPrice.value;
+
+    const inputDiscount = document.getElementById("InputDiscount");
+    const couponValue = inputDiscount.value;
+
+    const isCouponValueValid = function (coupon) {
+        return coupon.name === couponValue;
+    };
+    
+    const userCoupon = coupons.find(isCouponValueValid);
+    
+    if (!userCoupon) {
+        alert("El cupón " + couponValue + "no es válido");
+    } else {
+        const descuento = userCoupon.discount;
+        const precioConDescuento = calcularPrecioConDescuento(priceValue, descuento);
+
+        const resultP = document.getElementById("resultPrice");
+        resultP.innerText = "El precio con descuento son $" + precioConDescuento;
+        }
+}
