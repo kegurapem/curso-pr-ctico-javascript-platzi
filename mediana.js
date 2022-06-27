50e4a2d932b8344be6003d82536ee1db4f6781be
Kevin Harold Gutierrez Ramirez <harold.dev1993@gmail.com>
2022-06-26 22:54:15 -0500

    culminando promedio y mediana

--- mediana.js
+++ mediana.js
@@ -1,6 +1,19 @@
 function calcularMediaAritmetica(lista) {
-    const sumaLista = lista.reduce()
-    function (valorAcumulado = 0,)
+    // con for
+    // let sumaLista = 0;
+    // for (let i = 0; i < lista.length; i++) {
+    //     sumaLista = sumaLista + lista[i];
+    // }
+
+    // con reduce()
+    const sumaLista = lista.reduce(
+        function (valorAcumulado = 0, nuevoElemento) {
+            return valorAcumulado + nuevoElemento;
+        }
+    );
+
+    const promedioLista = sumaLista / lista.length;
+    return promedioLista
 }
 
 const lista1 = [100, 200, 500, 4000000];
@@ -19,9 +32,10 @@ let mediana;
 
 if (esPar(lista1.length)) {
     const elemento1 = lista1[mitalLista1];
-    const elemento2 = lista1[mitalLista1] - 1;
+    const elemento2 = lista1[mitalLista1- 1];
 
-    const 
+    const promedioElemento1y2 = calcularMediaAritmetica([elemento1, elemento2]);
+    mediana = promedioElemento1y2;
 } else {
     mediana = lista1[mitalLista1];
 }
