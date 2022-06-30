2c58e4473e187acc0a96b6e087c5454d91331037
Kevin Harold Gutierrez Ramirez <harold.dev1993@gmail.com>
2022-06-30 16:55:13 -0500

    culminamos el reto de la mediana ya gregamos los archivos de moda

--- mediana.js
+++ mediana.js
@@ -1,11 +1,14 @@
-function calcularMediaAritmetica(lista) {
-    // con for
-    // let sumaLista = 0;
-    // for (let i = 0; i < lista.length; i++) {
-    //     sumaLista = sumaLista + lista[i];
-    // }
 
-    // con reduce()
+
+
+const numberArray = [40, 1, 5, 200];
+
+function compareNumbers(a, b) {
+    return a - b;
+  }
+
+// ---------------------------------------
+function calcularMediaAritmetica(lista) {
     const sumaLista = lista.reduce(
         function (valorAcumulado = 0, nuevoElemento) {
             return valorAcumulado + nuevoElemento;
@@ -16,26 +19,63 @@ function calcularMediaAritmetica(lista) {
     return promedioLista
 }
 
-const lista1 = [100, 200, 500, 4000000];
+// const lista1 = [100, 200, 900, 500, 300, 800];
 
-const mitalLista1 = parseInt(lista1.length / 2);
+// function compareNumbers(a, b) {
+//     return a - b;
+//   }
 
-function esPar(numerito) {
-    if (numerito % 2 === 0) {
-        return true;
-    } else {
-        return false;
+// const listaOrdenada = lista1.sort(compareNumbers);
+
+// const mitalLista1 = parseInt(lista1.length / 2);
+
+// function esPar(numerito) {
+//     if (numerito % 2 === 0) {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
+
+// let mediana;
+
+// if (esPar(lista1.length)) {
+//     const elemento1 = lista1[mitalLista1];
+//     const elemento2 = lista1[mitalLista1 - 1];
+
+//     const promedioElemento1y2 = calcularMediaAritmetica([elemento1, elemento2]);
+//     mediana = promedioElemento1y2;
+// } else {
+//     mediana = lista1[mitalLista1];
+// }
+
+function calcularMediana(lista) {
+    function compareNumbers(a, b) {
+        return a - b;
+      }
+    
+    const listaOrdenada = lista.sort(compareNumbers);
+    
+    const mitalOrdenada = parseInt(listaOrdenada.length / 2);
+
+    function esPar(numerito) {
+        if (numerito % 2 === 0) {
+            return true;
+        } else {
+            return false;
+        }
     }
-}
 
-let mediana;
+    let mediana;
 
-if (esPar(lista1.length)) {
-    const elemento1 = lista1[mitalLista1];
-    const elemento2 = lista1[mitalLista1- 1];
+    if (esPar(listaOrdenada.length)) {
+        const elemento1 = listaOrdenada[mitalOrdenada];
+        const elemento2 = listaOrdenada[mitalOrdenada - 1];
 
-    const promedioElemento1y2 = calcularMediaAritmetica([elemento1, elemento2]);
-    mediana = promedioElemento1y2;
-} else {
-    mediana = lista1[mitalLista1];
+        const promedioElemento1y2 = calcularMediaAritmetica([elemento1, elemento2]);
+        mediana = promedioElemento1y2;
+    } else {
+        mediana = listaOrdenada[mitalOrdenada];
+    }
+    return mediana;
 }
