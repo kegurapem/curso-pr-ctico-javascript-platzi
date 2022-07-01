26dc400a08817feca5faf9e239d20346d0067bbc
Kevin Harold Gutierrez Ramirez <harold.dev1993@gmail.com>
2022-07-01 17:03:56 -0500

    agregando infor

---
+++ objetos/objetos.js
@@ -0,0 +1,18 @@
+const kevin = {
+    name: 'kevin',
+    apellido: 'gutirrez',
+    edad: 29
+}
+
+const kegura = {
+    name: 'kegura',
+    apellido: 'gutirrez',
+    edad: 4
+}
+
+function impimirNombreEdad(persona) {
+    const nombre = persona.name;
+    console.log(nombre);
+}
+
+impimirNombreEdad(kevin);
