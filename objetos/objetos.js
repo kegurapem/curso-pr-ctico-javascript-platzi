8ca2365d525f1a744787037e63c1f0a2dfe155b5
Kevin Harold Gutierrez Ramirez <harold.dev1993@gmail.com>
2022-07-02 00:11:02 -0500

    estudiando objetos

--- objetos/objetos.js
+++ objetos/objetos.js
@@ -1,6 +1,6 @@
 const kevin = {
     name: 'kevin',
-    apellido: 'gutirrez',
+    apellido: 'gutierrez',
     edad: 29
 }
 
@@ -11,8 +11,41 @@ const kegura = {
 }
 
 function impimirNombreEdad(persona) {
-    const nombre = persona.name;
+    const nombre = persona.name.toUpperCase();
     console.log(nombre);
 }
 
+function impimirNombreEdad2({name}) {
+    const nombre = name.toUpperCase();
+    console.log(nombre);
+}
+
+function impimirNombreEdad3({name}) {
+    console.log(name.toUpperCase());
+}
+
+function impimirNombreEdadFinal(persona) {
+    var {name} = persona;
+    var {edad} = persona;
+    console.log(`hola mi nombre es ${name} y tengo ${edad} anhos`);
+}
+
 impimirNombreEdad(kevin);
+impimirNombreEdad(kegura);
+
+impimirNombreEdad2(kevin);
+impimirNombreEdad2(kegura);
+
+impimirNombreEdad3(kevin);
+impimirNombreEdad3(kegura);
+
+impimirNombreEdadFinal(kevin);
+impimirNombreEdadFinal(kegura);
+
+//* Los objetos en js se modifican desde una función, por ende, si no queremos modificar debemos retornar un nuevo objeto modificado.
+function cumpleAnos(persona) {
+    return {
+        ...persona,
+        edad: persona.edad + 1,
+    }
+}
