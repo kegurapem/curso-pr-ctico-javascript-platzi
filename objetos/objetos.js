d7929b6c4b1f7a2ad54309e68b42cbc92524d6b1
Kevin Harold Gutierrez Ramirez <harold.dev1993@gmail.com>
2022-07-03 01:07:18 -0500

    agregando info

--- objetos/objetos.js
+++ objetos/objetos.js
@@ -49,3 +49,56 @@ function cumpleAnos(persona) {
         edad: persona.edad + 1,
     }
 }
+
+//*-------------------------------------------------------------------------------------
+function mayorDeEdad(persona) {
+    if (persona.edad >= 18) {
+        console.log(`${persona.name} tiene ${persona.edad} y es mayor de edad`)
+    } else {
+        console.log(`${persona.name} tiene ${persona.edad} y es menor de edad`)
+    }
+}
+
+//*Funciones que retornan valores-------------------------------------------------------
+const MAYORIA_DE_EDAD = 18;
+
+function esMayorDeEdad(persona) {
+    return persona.edad >= MAYORIA_DE_EDAD;
+}
+
+function imprimirSiEsMayorDeEdad(persona) {
+    if (esMayorDeEdad(persona)) {
+        console.log(`${persona.name} tiene ${persona.edad} y es mayor de edad`)
+    } else {
+        console.log(`${persona.name} tiene ${persona.edad} y es menor de edad`)
+    }
+}
+
+//*Arror functions-------------------------------------------------------
+const MAYORIA_DE_EDAD2 = 18;
+
+//* function (persona) -> se le conoce como, función anónima
+//* Esta es una función anónima
+// var esMayorDeEdad2 = function (persona) {
+//     return persona.edad >= MAYORIA_DE_EDAD2;
+// }
+
+//* Esta es una arrow function
+var esMayorDeEdad2 = (persona) => {
+    return persona.edad >= MAYORIA_DE_EDAD2;
+}
+
+const esMayorDeEdad2 = function (persona) {
+    return persona.edad >= MAYORIA_DE_EDAD2;
+}
+
+const esMayorDeEdad2 = persona => persona.edad >= MAYORIA_DE_EDAD2;
+
+
+function imprimirSiEsMayorDeEdad2(persona) {
+    if (esMayorDeEdad2(persona)) {
+        console.log(`${persona.name} tiene ${persona.edad} y es mayor de edad`)
+    } else {
+        console.log(`${persona.name} tiene ${persona.edad} y es menor de edad`)
+    }
+}
