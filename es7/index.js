9a9ae58d72be7894204d1bfe0e9b1596c431b8a7
Kevin Harold Gutierrez Ramirez <harold.dev1993@gmail.com>
2022-07-11 17:50:01 -0500

    agregando cambios

---
+++ es7/index.js
@@ -0,0 +1,16 @@
+// Se agrego includes, se utiliza para saber la existencia de un elemento dentro de un arreglo.
+let numbers = [1,2,4,7,8];
+
+if (numbers.includes(10)) {
+    console.log('Si se encuentra el valor 7');
+} else {
+    console.log('No se encuentra.');
+}
+
+// Eleva a la potencia
+
+let base = 2;
+let exponent = 3;
+let result = base ** exponent;
+
+console.log(result);
