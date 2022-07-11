567644b1209a710accbf9bfa056c2706d16da35d
Kevin Harold Gutierrez Ramirez <harold.dev1993@gmail.com>
2022-07-11 17:51:02 -0500

    agregando mi configuracion de mi tema

---
+++ styleThemeCss.js
@@ -0,0 +1,81 @@
+{
+    "security.workspace.trust.untrustedFiles": "open",
+    "liveServer.settings.AdvanceCustomBrowserCmdLine": "/mnt/c/Program Files/Google/Chrome/Application/chrome.exe",
+    "liveServer.settings.CustomBrowser": "chrome:PrivateMode",
+    "liveServer.settings.donotShowInfoMsg": true,
+    "editor.minimap.enabled": false,
+    "workbench.colorTheme": "SynthWave '84",
+
+    "workbench.colorCustomizations": {
+        "[SynthWave '84]": {
+            "editor.background": "#312d2d"
+        }
+    },
+    "editor.tokenColorCustomizations": {
+    //     // "strings": "#FF0000"
+    //     // "functions": "#6AFCFE",
+    //     "functions": "#ff94ac",
+    //     "variables": "#e9e3e3",
+        "comments": "#85c084",
+    //     "keywords": "#6db8ed",
+        "textMateRules": [
+    //         {
+    //             "scope": "keyword.operator",
+    //             "settings": {
+    //                 "foreground": "#ff8800",
+    //                 "fontStyle": "bold"
+    //             }
+    //         },
+    //         {
+    //             "scope": "keyword.control",
+    //             "settings": {
+    //                 "foreground": "#ff8800",
+    //                 // "fontStyle": "bold"
+    //             }
+    //         },
+            {
+                "scope" : "variable.parameter",
+                "settings": {
+                    "foreground": "#f3f0ec",
+                    "fontStyle": ""
+                }
+            },
+            // {
+            //     "scope" : "string",
+            //     "settings": {
+            //         "foreground": "#eed237",
+            //         // "fontStyle": "bold"
+            //     }
+            // },
+            {
+                "scope" : "comment",
+                "settings": {
+                    "foreground": "#6bdeae",
+                    "fontStyle": ""
+                }
+            },
+            // {
+            //     "scope" : "meta",
+            //     "settings": {
+            //         "foreground": "#e9f4f1",
+            //         // "fontStyle": "bold"
+            //     }
+            // },
+            {
+                "scope" : "support",
+                "settings": {
+                    "foreground": "#2ee2ee",
+                    // "fontStyle": "bold"
+                }
+            },
+            {
+                "scope" : "constant.numeric",
+                "settings": {
+                    "foreground": "#e6a716",
+                    // "fontStyle": "bold"
+                }
+            }
+        ]
+    },
+    "window.zoomLevel": 2
+}
